5245b1d294f593e866b638997a9b2eddea01b92e
Daniel Madison <madisodr@email.sc.edu>
2015-05-30 00:15:36 -0400

    Started Binarysearch Algorithm

--- selection.cpp
+++ selection.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class QuickSelect
 {
+    public:
+        QuickSelect(vector<int>& A, int key)
+        {
+            cout << "QuickSelect\n";
+            search(A, 0, A.size(), key);
+        }
 
+        int search(vector<int>& A, int imin, int imax, int key)
+        {
+
+        }
 };
 
 class BinarySearch
 {
+    public:
+        BinarySearch(vector<int>& A, int key)
+        {
+            cout << "Binary Search\n";
+            search(A, 0, A.size(), key);
+        }
 
+        int search(vector<int>& A, int imin, int imax, int key)
+        {
+            if(A.size() == 0)
+                return 0;
+            
+            int mid = A.size() / 2;
+
+            if(A[mid] > key)
+                return search(A, imin, mid - 1, key);
+            else if(A[mid] < key)
+                return search(A, mid + 1, imax, key);
+            else
+                return mid;
+        }
 };
