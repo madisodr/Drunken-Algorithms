0a105e54a6076f6d4c150b252cd87fe512e3fe70
Daniel Madison <madisodr@email.sc.edu>
2015-08-27 13:28:27 -0400

    Added an example graph dataset and finished the graph class.

--- drunk.h
+++ drunk.h
@@ -1,20 +1,20 @@
 /* 
-* Author: Daniel R Madison
-* Copyright (C) 2015
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Author: Daniel R Madison
+ * Copyright (C) 2015
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <vector>
 #include <iostream>
@@ -23,13 +23,17 @@
 #define SORT_SIZE 100 // number of elements to use when populating vectors and arrays
 #define OUTPUT true // display the vector output or not in console.
 
+#define DATA_COMMENT "##"
+#define NODE_DELIM ":"
+#define NODE_LINK_DELIM ","
+
 using namespace std;
 
 /********************
-* output a vector of integers
-* TODO: print a vector of any object provided it has a class method to_string or is
-* a standard library type.
-* *****************/
+ * output a vector of integers
+ * TODO: print a vector of any object provided it has a class method to_string or is
+ * a standard library type.
+ * *****************/
 
 void printVector(std::vector<int>& A)
 {
@@ -63,4 +67,24 @@ int drunk_max(int x, int y)
         return x;
     else
         return y;
+}
+
+// trim from start
+static inline string &ltrim(string &s) 
+{
+    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+    return s;
+}
+
+// trim from end
+static inline string &rtrim(string &s) 
+{
+    s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+static inline string &trim(string &s) 
+{
+    return ltrim(rtrim(s));
 }
--- graphs.cpp
+++ graphs.cpp
@@ -18,59 +18,49 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <map>
 #include <string>
-#include <gtk/gtk.h>
+#include <iostream>
+#include <fstream>
 
 #include "drunk.h"
 
 using namespace std;
 
-class HTML_Output
-{
-}
-
-typedef vector<Node*> node_vector;
-typedef node_vector::iterator node_itr;
-
-typedef map<Node*, int> edge_vector;
-typedef edge_vector::iterator edge_itr;
-
 class Node
 {
     public:
-        Node(int /*id*/, string s)
-        {
-            this->label = s;
-        }
-
+        Node(string s){ this->label = s; }
         ~Node(){}
 
-        link(Node* n, int cost = 0) 
-        { 
-            edges[n] = cost; 
-        }
+        void link(string n, int cost = 0) { edges[n] = cost; }
+        void link(Node* n, int cost = 0) { edges[n->getLabel()] = cost; }
 
-        delink(Node* n) 
-        { 
-            edges.erase(n);
-        }
+        void delink(string n) { edges.erase(n); }
+        void delink(Node* n) { edges.erase(n->getLabel()); }
 
-        map<Node*, int> getEdges()
+        bool isLinked(Node* n)
         {
-            return this->edges;
+            map<string, int>::iterator it;
+            for(it = edges.begin(); it != edges.end(); ++it)
+            {
+                if(it->first == n->getLabel())
+                    return true;
+            }
+
+            return false;
         }
 
-        string getLabel()
-        {
-            return this->label;
-        }
-        
+        map<string, int> getEdges() { return this->edges; }
+        string getLabel() { return this->label; }
+
     private:
         string label;
-        map<Node*, int> edges;
+        map<string, int> edges;
 
-}
+};
+
 class Graph
 {
 
@@ -78,27 +68,43 @@ class Graph
         Graph(){}
         ~Graph(){}
 
-        addNode(Node* n)
+        void addNode(Node* n)
         {
             this->nodes.push_back(n);
         }
 
-        delNode(Node* n)
+        void delNode(Node* n)
         {
-            nodes.erase(std::remove(nodes.begin(), nodes.end(), n), nodes.end);
+            this->nodes.erase(remove(this->nodes.begin(), this->nodes.end(), n), this->nodes.end());
+        }
+
+        Node* getNodeByLabel(string n)
+        {
+            vector<Node*>::iterator it;
+            for(it = nodes.begin(); it != nodes.end(); ++it)
+            {
+                if((*it)->getLabel() == n)
+                    return *it;
+            }
+
+            return NULL;
         }
 
         void output()
         {
-            for(node_itr itn = nodes.begin(); itn != nodes.end(); ++itn)
-            {
-                cout << itn->getLabel() << "\n";
-                map<Node*, int> edges = itn->getEdges();
+            vector<Node*>::iterator itn;
+            map<string, int>::iterator ite;
 
-                for(edge_itr ite = edges.begin(); ite != edges.end(); ++ite)
-                {
-                    cout << ite->first->getLabel() << " => " << ite->second << "\n";
-                }
+            for(itn = nodes.begin(); itn != nodes.end(); ++itn)
+            {
+                cout << "Node Label: " << (*itn)->getLabel() << "\n";
+                map<string, int> edges = (*itn)->getEdges();
+
+                cout << "Connected Nodes => count(" << edges.size() << ")\n";
+                for(ite = edges.begin(); ite != edges.end(); ++ite)
+                    cout << ite->first << "(" << ite->second << ")\n";
+
+                cout << "\n";
             }
         }
 
@@ -108,5 +114,62 @@ class Graph
 
 int main(int argc, char*argv[])
 {
+ 
+    size_t pos;
+    string token, line;
 
+    // File to retrieve data from.
+    // TODO: make interactive way to imput data. 
+    ifstream inFile ("data/graph1.txt");
+
+    // create a new graph
+    Graph* graph = new Graph();
+
+    if(inFile.is_open()) // did we open the file?
+    {
+        while(getline(inFile, line))
+        {
+
+            // ignore lines that are comments or blank
+            if((line.compare(0,2,"##") == 0) || (line.length() == 0))
+                continue;
+            
+            // grab the node's label and create the node.
+            string nodeLabel = line.substr(0, line.find(NODE_DELIM));
+            Node* node = new Node(trim(nodeLabel));
+            
+            // Grab the rest of the line. These are the edges. 
+            string nodeEdges = line.substr(line.find(NODE_DELIM)+1, line.length()-1);
+
+            // if its an unconnected node then we dont need to do anything more.
+            if(nodeEdges.length() == 0)
+            {
+                graph->addNode(node);
+                continue;
+            }
+
+            // split the rest of the line and extract al the edges
+            while((pos = nodeEdges.find(NODE_LINK_DELIM)) != string::npos)
+            {
+                token = nodeEdges.substr(0, pos);
+                node->link(trim(token));
+                nodeEdges.erase(0, pos + 1);
+            }
+
+            // the while loop stops at the last element but we still need to
+            // add it to the node. 
+            node->link(trim(nodeEdges));
+
+            // add our new node to the graph
+            graph->addNode(node);
+        }
+
+        inFile.close();
+    }
+    else 
+        cout << "Unable to open file.\n";
+
+    graph->output();
+
+    return 0;
 }
