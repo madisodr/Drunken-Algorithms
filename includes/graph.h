51276c1d9c097c686e29cdb348927717d03ce952
Daniel Madison <madisodr@email.sc.edu>
2016-07-25 16:18:49 -0400

    I need to figure out how I want to store graphs...

--- includes/graph.h
+++ includes/graph.h
@@ -1,47 +1,60 @@
 /* 
-* Author: Daniel R Madison
-* Copyright (C) 2016
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Author: Daniel R Madison
+ * Copyright (C) 2016
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <vector>
 
 using namespace std;
 
-typedef Matrix std::vector<std::vector<Node*> >;
+typedef Matrix std::vector<std::vector<int> >;
 
 class Node {
 	public:
+
+
 		Node(int idx, int data);
 		~Node();
+		bool addEdge(Node* n);
+		bool remEdge(Node* n);
+		bool hasEdge(Node* n);
+		std::vector<Node*> getEdges() { return m_edges; }
 
+	private:
 		int m_idx;
 		int m_data;
-};
+		std::vector<Node*> m_edges;
+}
 
-class AdjacencyMatrix {
+
+
+class Graph {
 	public:
-		AdjacencyMatrix();
-		AdjacencyMatrix(int n);
-		~AdjacencyMatrix();
+		Graph();
+		Graph(Graph &g);
+		~Graph();
+
 		bool addNode(Node* n);
-		bool removeNode(Node* n);
-		bool removeNode(int idx);
-		bool addEdge(Node* a, Node* b);
-		bool removeEdge(Node* a, Node* b);
-		Node* getNode(int idx);
+		bool addNode(int data);
+
+		bool remNode(Node* n);
+		bool remNode(int data);
+
 	private:
-		Matrix m_matrix;
+		std::vector<Node*> m_nodes;
+		int m_nodeCount;
+
 };
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,20 +1,20 @@
 /* 
-* Author: Daniel R Madison
-* Copyright (C) 2016
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Author: Daniel R Madison
+ * Copyright (C) 2016
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <iostream>
 #include <vector>
@@ -23,6 +23,7 @@
 
 using namespace std;
 
+
 Node::Node(int idx, int data) {
 	m_idx = idx;
 	m_data = data;
@@ -30,106 +31,77 @@ Node::Node(int idx, int data) {
 
 Node::~Node() {}
 
-AdjacencyMatrix::AdjacencyMatrix() {
-	// empty constructor	
+bool Node::addEdge(Node* n) {
+	if(hasEdge(n))
+		return false;
+
+	m_edges.push_back(n);
+	return true;
 }
 
-AdjacencyMatrix::AdjacencyMatrix(int n) {
-	m_matrix.reserve(n);
-	for(int i = 0; i < n; i++) {
-		vector<Node*> tmp (n);
-		m_matrix[i] = tmp;
+bool Node::remEdge(Node* n) {
+	if(!hasEdge(n))
+		return false;
+
+	auto it = std::find(m_edges.begin(), m_edges.end(), n);
+	if(it != m_edges.end()) {
+		using std::swap;
+		swap(*it, m_edges.back());
+		m_edges.pop_back();
+		return true
 	}
+
+	return false;
 }
 
-AdjacencyMatrix::~AdjacencyMatrix() {
-	for(uint i = 0; i < m_matrix.size(); i++) {
-		for(uint j = 0; j < m_matrix[i].size(); j++) {
-			delete m_matrix[i][j];
-		}
+bool Node::hasEdge(Node* n) {
+	auto it = std::find(m_edges.begin(), m_edges.end(), n);
+
+	if(it != m_edges.end())
+		return true;
+	else
+		return false;
+}
+
+Graph::Graph() {
+	m_nodeCount = 0;
+}
+
+Graph::~Graph() {
+	for(int i = 0; i < m_nodes; i++) {
+		delete m_nodes[i];
 	}
+
+	m_nodeCount = 0;
 }
 
-AdjacencyMatrix::addNode(Node* n) {
-	
+bool addNode(Node* n) {
+	if(hasNode(n))
+		return false;
+
+	m_nodes.push_back(n);
+	m_nodeCount++;
+	return true;
 }
 
-class Graph {
-    public:
-        // Construct a new graph with number v verticies. 
-        Graph(int _v) { 
-            this->v = _v;
-            adj = new list<int>[v];
-        }
-        void addEdge(int v, int w) {
-            adj[v].push_back(w); // add w to v's list
-        }
+bool addNode(int data) {
+	m_nodeCount++;
+	Node* n = new Node(m_nodeCount, data);
+	m_nodes.push_back(n);
+	return true;
+}
 
-        void BFS(int s) {
-            // Mark a set of vertices as not visted
-            bool* checked = new bool[];
-            for(int i = 0; i < v; i++) 
-                checked[i] = false;
+bool remNode(Node* n) {
+	if(!hasNode(n)) 
+		return false;
 
-            list<int> queue;
-            checked[s] = true;
-            queue.push_back(s);
-            list<int>::iterator i;
-            while(!queue.empty()) {
-                s = queue.front();
-                cont << s << " ";
-                queue.pop_front();
+	auto it = find::(m_nodes.begin(), m_nodes.end(), n);
+	if(it != m_edges.end()) {
+		using std::swap;
+		swap(*it, m_edges.back());
+		m_edges.pop_back();
+		return true;
+	}
 
-                for(i = adj[s].begin(); i != adj[s].end(); ++i) {
-                    if(!checked[*i]) {
-                        checked[*i] = true;
-                        queue.push_back(*i);
-                    }
-                }
-            }
-        }
-
-
-        // Main method for depth first search traversal
-        void DFS(int s) {
-            bool* checked = new bool[];
-            for(int i = 0; i < v; i++)
-                checked[i] = false;
-
-            DFS(v, checked);
-
-        }
-
-        // The depth first recursion
-        void DFS(int s, bool checked[]){
-            checked[v] = true;
-            cout << v << " ";
-            
-            list<int>::iterator i;
-            for(i adj[v].begin(); i != adj[v].end(); i++) {
-                if(!checked[*i])
-                    DFS(v, checked);
-            }
-        }
-
-        void AStar() {}
-        void Dijkstras() {}
-    private:
-        int v;
-        list<int> *adj;
-};
-
-int main() {
-    Graph graph(4);
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 2);
-    graph.addEdge(1, 2);
-    graph.addEdge(2, 0);
-    graph.addEdge(2, 3);
-    graph.addEdge(3, 3);
-
-    cout << "Breadth First Search\n";
-    graph.BFS();
-    cout << "Depth First Search\n";
-    graph.DFS();
+	return false;
 }
